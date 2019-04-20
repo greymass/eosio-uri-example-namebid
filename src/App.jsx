a4f45bb7065ff72710b78f0ae71510ac76cea36d
dafuga <danielfugere28@gmail.com>
2019-04-20 14:32:17 +0800

    refactor: renamed files for BiddingForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Segment, Message } from 'semantic-ui-react';
 
-import DonationButtonPreviewer from './App/DonationButtonPreviewer'
-import DonationForm from './App/DonationForm';
-import URIPreviewer from './App/URIPreviewer';
+import BiddingForm from './App/BiddingForm';
+import URIButton from './App/URIButton';
 
 class App extends Component {
   state = {};
@@ -25,41 +24,18 @@ class App extends Component {
           basic
           style={segmentStyle}
         >
-          <Segment basic style={{ marginTop: 100, minHeight: window.innerHeight - 140 }}>
-            <Segment basic style={{ width: 400 }}>
-              <Message
-                success
-                style={{ marginBottom: 100 }}
-                content="
-                  This is an EOSIO uri example project created by Greymass to allow any website owner to generate
-                  a donation link and/or button. To start using this tool simply fill up the form below.
-                "
-              />
-              <DonationForm onStateChange={this.onStateChange} />
-            </Segment>
+          <Segment basic style={{ marginTop: 100, minHeight: window.innerHeight - 140, width: 400 }}>
+            <Message
+              success
+              style={{ marginBottom: 100 }}
+              content="
+                This is an EOSIO uri example project created by Greymass to allow any website owner to generate
+                a donation link and/or button. To start using this tool simply fill up the form below.
+              "
+            />
+            <BiddingForm onStateChange={this.onStateChange} />
             {eosioURI && (
-              <Segment>
-                <URIPreviewer key='uri_previewer' eosioURI={eosioURI} />
-                {!generatingButton && (
-                  <a
-                    key='generate_button_link'
-                    onClick={() => this.setState({ generatingButton: true })}
-                    style={{ cursor: 'pointer' }}
-                  >
-                    Generate a button for your website
-                  </a>
-                )}
-                {generatingButton && (
-                  <React.Fragment>
-                    <hr />
-                    <DonationButtonPreviewer
-                      donationAmount={donationAmount}
-                      eosioURI={eosioURI}
-                      key='button_previewer'
-                    />
-                  </React.Fragment>
-                )}
-              </Segment>
+              <URIButton key='uri_previewer' eosioURI={eosioURI} />
             )}
           </Segment>
           <div style={{ margin:'auto', width: 200} }>
